cb137b0d39d5d3aa1b3977e80f170b37e8ff6fde
Flavio Rodrigo De Souza Ribeiro <125625270+Flavim-rsr@users.noreply.github.com>
2023-10-17 20:15:44 -0300

    Correção de id

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let buscarFilme = async () => {
-    let title = document.getElementById("title").value;      //pega oque foi digitado no html para buscar na API
+    let title = document.getElementById("Title").value;      //pega oque foi digitado no html para buscar na API
     let resposta = await fetch(`http://www.omdbapi.com/?t=${title}&apikey=54ddc12`);
     let dados = await resposta.json();
     //tratamento de erro caso pesquise um filme invalido
@@ -9,10 +9,10 @@ let buscarFilme = async () => {
         alert(`Filme não encontrado, digite outro`)
         return
     }
-    document.getElementById("runTime").value = dados.Runtime;
-    document.getElementById("actors").value = dados.Actors;
-    document.getElementById("language").value = dados.Language; 
-    document.getElementById("director").value = dados.Director; 
-    document.getElementById("year").value = dados.Year; 
-    document.getElementById("poster").src = dados.Poster;
+    document.getElementById("RunTime").value = dados.Runtime;
+    document.getElementById("Actors").value = dados.Actors;
+    document.getElementById("Language").value = dados.Language; 
+    document.getElementById("Director").value = dados.Director; 
+    document.getElementById("Year").value = dados.Year; 
+    document.getElementById("Poster").src = dados.Poster;
 }
